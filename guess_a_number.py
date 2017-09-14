cbc5e102089c438925f4e71af6ada0e5c6a68a31
Blom <eblom2629@greenville.k12.sc.us>
2017-09-14 14:24:39 -0400

    Added in telling what the number was when you lose.

--- guess_a_number.py
+++ guess_a_number.py
@@ -4,12 +4,14 @@ high = 100
 low = 1
 limit = 10
 
+#intro to game
 rand = random.randrange(low,high)
 print("I'm thinking of a number from " + str(low) + " to " + str(high) + ".");
 
 guess = -1
 tries = 0
 
+#main game loop
 while guess != rand and tries < limit:
     guess = input("Take a guess: ")
     guess = int(guess)
@@ -18,11 +20,13 @@ while guess != rand and tries < limit:
         print("You guessed too low.")
     elif guess > rand:
         print("You guessed too high.")
+        
     tries += 1
     print("You have " + str(limit - tries) + " tries left.")
 
 
+#game ends
 if tries == limit:
-    print("You lose.")
+    print("You lose. The number was " + str(rand) + ".")
 else:
     print("You win.")
