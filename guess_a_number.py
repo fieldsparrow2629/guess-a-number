a032932f303c9eeb1d09a27670e9edceb82f95d5
Blom <eblom2629@greenville.k12.sc.us>
2017-09-14 14:17:37 -0400

    Added in a tries counter and tries limit

--- guess_a_number.py
+++ guess_a_number.py
@@ -2,12 +2,15 @@ import random
 
 high = 100
 low = 1
+limit = 10
+
 rand = random.randrange(low,high)
 print("I'm thinking of a number from " + str(low) + " to " + str(high) + ".");
 
 guess = -1
+tries = 0
 
-while guess != rand:
+while guess != rand and tries < limit:
     guess = input("Take a guess: ")
     guess = int(guess)
     
@@ -15,7 +18,11 @@ while guess != rand:
         print("You guessed too low.")
     elif guess > rand:
         print("You guessed too high.")
-    else:
-        print("You got it!")
+    tries += 1
+    print("You have " + str(limit - tries) + " tries left.")
 
-print("Game over")
+
+if tries == limit:
+    print("You lose.")
+else:
+    print("You win.")
