2e1715993932df1470722c1b1dbfe3f44f907038
Blom <eblom2629@greenville.k12.sc.us>
2017-09-22 14:22:39 -0400

    Added in splash screen, spacing between lines, limit calculator, and a way to input range

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,32 +1,102 @@
+
+import math
 import random
 
 high = 100
-low = 1
-limit = 10
+low = 0
 
-#intro to game
-rand = random.randrange(low,high)
-print("I'm thinking of a number from " + str(low) + " to " + str(high) + ".");
+limit = 0
 
-guess = -1
-tries = 0
 
-#main game loop
-while guess != rand and tries < limit:
-    guess = input("Take a guess: ")
-    guess = int(guess)
+def splash_screen():
+    print(""" _____                        ___    _   _                 _               
+|  __ \                      / _ \  | \ | |               | |              
+| |  \/_   _  ___  ___ ___  / /_\ \ |  \| |_   _ _ __ ___ | |__   ___ _ __ 
+| | __| | | |/ _ \/ __/ __| |  _  | | . ` | | | | '_ ` _ \| '_ \ / _ \ '__|
+| |_\ \ |_| |  __/\__ \__ \ | | | | | |\  | |_| | | | | | | |_) |  __/ |   
+ \____/\__,_|\___||___/___/ \_| |_/ \_| \_/\__,_|_| |_| |_|_.__/ \___|_|   
+                                                                           """)
+
+def choose_bound():
+    global high
+    global low
+    print("What will the highest number be?")
+    high = int(input())
+    print("What will the lowest number be?")
+    low = int(input())
+
+   
+def get_limit():
+    global limit
+    limit = math.log(high,2);
+    limit = math.ceil(limit)
+    print("You are limited to " + str(limit) + " tries.")
+    print()
     
+def get_guess():
+    while True:
+        guess = input("Guess a number: ")
+
+        if guess.isnumeric():
+            guess = int(guess)
+            return guess
+        else:
+            print("You must enter a number.")
+
+def pick_number():
+    print("I'm thinking of a number from " + str(low) + " to " + str(high) + ".")
+    print()
+    return random.randint(low,high)
+
+def check_guess(guess, rand):
     if guess < rand:
         print("You guessed too low.")
     elif guess > rand:
         print("You guessed too high.")
-        
-    tries += 1
-    print("You have " + str(limit - tries) + " tries left.")
+    print()
+
+def show_result(guess,rand):
+    if guess == rand:
+        print("Congrats you win!")
+    else:
+        print("Sorry, you lose, the number was " + str(rand) + ".")
+
+def play_again():
+    while True:
+        decision = input("would you like to play again? (y/n) ")
+
+        if decision.lower() == 'y' or decision.lower() == 'yes' :
+            print()
+            return True
+        elif decision.lower() == 'n' or decision.lower() == 'no':
+            print()
+            return False
+        else:
+            print("Please enter 'y' or 'n'.")
+
+def play():
+    guess = -1
+    tries = 0
+
+    rand = pick_number()
+
+    while guess != rand and tries < limit:
+        guess = get_guess()
+        check_guess(guess,rand)
+
+        tries += 1
+
+    show_result(guess,rand)
+    
+splash_screen()
 
 
-#game ends
-if tries == limit:
-    print("You lose. The number was " + str(rand) + ".")
-else:
-    print("You win.")
+
+playing = True
+
+while playing:
+    choose_bound()
+    get_limit()
+    play()
+    playing = play_again()
+
